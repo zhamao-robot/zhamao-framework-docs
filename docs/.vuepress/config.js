08e3d468e19c21fb937386a91f5d2cfec51ace25
whale <jesse2061@outlook.com>
2020-05-10 01:32:09 +0800

    update Docs: 更新进阶

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: `/logo.png` }],
         ['link', { rel: 'manifest', href: '/manifest.json' }],
-        ['meta', { name: 'theme-color', content: '#ffffff' }],
+        ['meta', { name: 'theme-color', content: '#5546a3' }],
         ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
         ['meta', { name: 'msapplication-TileColor', content: '#00aba9' }]
     ],
